0385fbfb7c41aa56ad54926714f4e98abd4b4ad5
Aaron Dufall <aald212@gmail.com>
2015-12-05 13:20:47 -0800

    Add react and configure hotloading for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }, { 
       test: /\.css$/,
       loader: 'style!css!autoprefixer?browsers=last 2 versions'
@@ -21,6 +25,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
