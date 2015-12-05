a9182d48d5b3792d48d69634660833308df3982a
Aaron Dufall <aald212@gmail.com>
2015-12-05 12:48:18 -0800

    Add ES6 support

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
