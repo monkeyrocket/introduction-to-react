64ae47a3951a8b726907c5adf8c2c4a531fd9b3b
Aaron Dufall <aald212@gmail.com>
2015-12-05 22:18:29 -0800

    Add id to div for render function to connect to

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,7 +2,7 @@ import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const doc = jsdom.jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
 const win = doc.defaultView;
 
 global.document = doc;
