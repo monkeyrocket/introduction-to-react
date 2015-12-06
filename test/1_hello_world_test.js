487e01d436bb0024a041d8a71ce8e587ad1c2a6b
Aaron Dufall <aald212@gmail.com>
2015-12-05 22:20:13 -0800

    Add test for first challenge

---
+++ test/1_hello_world_test.js
@@ -0,0 +1,10 @@
+import React from 'react/addons';
+import '../src/index';
+import {expect} from 'chai';
+
+
+describe('Rendering', () => {
+  it('show hello world', () => { 
+    expect(global.document.getElementsByTagName("H1")[0].innerHTML).to.equal('Hello World!')    
+  })
+})
